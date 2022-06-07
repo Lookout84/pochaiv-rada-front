7854c2075d2b8cdfe3a19af04c549faa5c7361ec
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2022-06-08 00:15:27 +0300

    bootstrap navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 // import Article from './components/Articles';
 import ArticlesList from './components/ArticlesList/ArticlesList';
 import articles from './data/articles.json';
-import Container from './components/Container/Container';
+// import Container from './components/Container/Container';
+// import AppBar from './components/AppBar/AppBar';
+import Layout from './components/Layout/Layout';
 
 console.log(articles);
 
 const App = () => {
     return (
-        <Container>
+        <Layout>
             <h1>Головна сторінка</h1>
             <ArticlesList articles={articles} />;
-        </Container>
+        </Layout>
     );
 };
 
