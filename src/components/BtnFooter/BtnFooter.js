012b4743a7a048c938f9fc1ddf3aa754d0b7ab63
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2022-06-09 17:18:44 +0300

    update footer

--- src/components/BtnFooter/BtnFooter.js
+++ src/components/BtnFooter/BtnFooter.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import s from './BtnFooter.module.css';
+import iconSend from '../../images/icons/facebook.png';
 
 const BtnFooter = () => {
     return (
         <button className={s.Btn_subscription} type="submit">
             <span className={s.Btn_text}>Подписаться</span>
-            <svg className={s.Icon_send} width="24" height="24">
-                <use href="./images/icons/sprite.svg#icon-send"></use>
-            </svg>
+            <img className={s.Icon_send} width="24" height="24" alt="" src={iconSend} />
         </button>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// import { ReactSVG } from 'react-svg';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
