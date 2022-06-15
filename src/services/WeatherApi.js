fbb8c60d72509a0d3421c1c6e1d4675bcf0f30ac
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2022-06-15 17:06:55 +0300

    update

--- src/services/WeatherApi.js
+++ src/services/WeatherApi.js
@@ -1,6 +1,6 @@
 let apiKey = `b17a2dddb01d7481fea6373f92c2e546`;
 let baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=`;
-// let query = "Kyiv";
+
 
 function getFetch(query) {
     let url = `${baseUrl}${query}&units=metric&appid=${apiKey}`;
