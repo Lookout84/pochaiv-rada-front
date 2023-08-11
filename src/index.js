badd89e9140c06a953abad1d86662349f7c466e8
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2023-08-11 14:41:40 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,12 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App analytics={analytics} />
+    <App />
+    {/* <App analytics={analytics} /> */}
   </React.StrictMode>,
 );
